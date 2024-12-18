d5ba7394b76e9944a38f88e744679912a664949a
codingjordi <jordisaperascg@gmail.com>
2024-12-18 17:55:51 +0100

    fix-api-fetch-get-request

--- src/api/fountains.api.js
+++ src/api/fountains.api.js
@@ -1,7 +1,5 @@
+import axios from "axios"
 
 export const getFountainsRequest = async () => {
-    return await fetch('https://main.d1udmiicb2cjk1.amplifyapp.com/fountains')
+    return await axios.get(import.meta.env.VITE_MAPBOX_TOKEN)
 }
-
-
-
